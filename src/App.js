a7ee483ca6c705f6ceae6d90cfbe07faf5e2f016
noman27 <blt.normanbruce@yahoo.com>
2023-08-27 19:43:07 +0600

    bekar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Sections/Header/Header/Header";
+import Myself from "./Sections/Myself/Myself";
 
 function App() {
   return (
     <div className="container mx-auto">
       <Header></Header>
+      <Myself></Myself>
     </div>
   );
 }
--- src/Sections/Myself/Myself.js
+++ src/Sections/Myself/Myself.js
@@ -1,7 +1,28 @@
 import React from "react";
+import mypic from "../../assets/rsz_mypic.jpeg";
 
 const Myself = () => {
-  return <div></div>;
+  const about_me =
+    "Hello there, It's Adbullah Al Noman here.I am a passionate programmer who loves to develop web apps and learn new technologies.I can handle both frontend and backend development of a project also manage Database either its SQL/NoSQL.";
+  return (
+    <div className="relative w-[100%] mt-5 lg:h-screen flex justify-center">
+      {" "}
+      <div className="absolute mt-[25px] top-[25%] flex flex-col justify-center lg:flex-row-reverse">
+        <div className=" mb-[50px]">
+          <img
+            className=" rounded-full mb-7 lg:mt-[-50px] h-[400px] w-[400px]"
+            src={mypic}
+            alt=""
+          />
+        </div>
+
+        <div className=" w-[100%] lg:w-[35%] text-start">
+          <p className=" text-3xl">Full Stack Web Developer here</p>
+          <p className=" ">{about_me}</p>
+        </div>
+      </div>{" "}
+    </div>
+  );
 };
 
 export default Myself;
