8a6fa40489c6d2ba68893d30218cf1d0098c159c
noman5945 <abdullahnoman.5945@gmail.com>
2023-11-20 18:33:50 +0600

    CV download

--- src/Sections/ShortDescription/ShortDescription.js
+++ src/Sections/ShortDescription/ShortDescription.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 const ShortDescription = () => {
+  const url = "/CV/CV_ABDULLAH_AL_NOMAN.pdf";
+  const downloadCV = (url) => {
+    const filename = "CV_NOMAN.pdf";
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", filename);
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
   return (
     <div className=" block max-h-screen lg:h-[650px] bg-orange-50">
       <div className=" container flex flex-col align-middle justify-center  mx-auto pt-[12%]">
         <p className=" text-center text-lg">Hi,I am Abdullah Al Noman</p>
         <p className=" text-center text-4xl font-bold">I am a Developer</p>
-        <button className=" my-2 btn btn-success w-40 mx-[45%] ">
+        <button
+          onClick={() => downloadCV(url)}
+          className=" my-2 btn btn-success w-40 mx-[45%] "
+        >
           Download CV
         </button>
       </div>
