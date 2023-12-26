a52edb3b41ff3b49f1a179b9c116b41d76088623
noman5945 <abdullahnoman.5945@gmail.com>
2023-12-26 11:36:15 +0600

    service sector done now the projects

--- src/Sections/Services/ServiceItems.js
+++ src/Sections/Services/ServiceItems.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const ServiceItems = ({ item }) => {
-  const { style, Title, Msg, img } = item;
+  const { style, Title, img } = item;
   return (
-    <div style={style} className=" my-5 h-1/2">
-      <div className=" block">
-        <p className=" text-2xl font-bold">{Title}</p>
-        <p>{Msg}</p>
-      </div>
-      <div className="">
-        <img className=" h-[100%] w-[100%]" src={img} alt=""></img>
+    <div style={style} className=" items-center flex flex-col p-3   rounded-md">
+      <img
+        className=" rounded-md h-[50px] w-[50px] lg:h-[100px] lg:w-[100px]"
+        src={img}
+        alt=""
+      ></img>
+
+      <div className=" block p-4">
+        <p className="  text-base lg:text-lg font-bold">{Title}</p>
       </div>
     </div>
   );
--- src/Sections/Services/ServicesMain.js
+++ src/Sections/Services/ServicesMain.js
@@ -6,9 +6,9 @@ const ServicesMain = () => {
     {
       Title: "Front-End Development",
       Msg: " Armed with a solid foundation in web development technologies, I am eager to contribute my skills to create visually appealing and seamless interfaces.",
-      img: "https://assets-global.website-files.com/5e4c6b4b7ed0a2e77458ce3d/638a92cf349604581a35fe75_263a75529a1752b75d64f9f21fd07c92-3-2.jpeg",
+      img: "https://static.vecteezy.com/system/resources/previews/007/571/212/non_2x/frontend-development-line-icon-vector.jpg",
       style: {
-        backgroundColor: "#F08080",
+        backgroundColor: "#FA8072",
         display: "flex",
         flexDirection: "row",
       },
@@ -16,11 +16,31 @@ const ServicesMain = () => {
     {
       Title: "Back-End Development",
       Msg: "Proficient in backend development, I leverage the power of Node.js and .NET to build robust and scalable server-side solutions. With a keen eye for optimizing performance and ensuring seamless data flow, I bring a versatile skill set to deliver efficient and reliable backend functionalities.",
-      img: "https://plopdo.com/wp-content/uploads/2021/10/What-is-back-end-development-2.jpg",
+      img: "https://static.vecteezy.com/system/resources/previews/016/865/530/non_2x/backend-development-line-gradient-circle-background-icon-vector.jpg",
       style: {
         backgroundColor: "#FA8072",
         display: "flex",
-        flexDirection: "row-reverse",
+        flexDirection: "row",
+      },
+    },
+    {
+      Title: "Communication and Colaboration",
+      Msg: "Proficient in backend development, I leverage the power of Node.js and .NET to build robust and scalable server-side solutions. With a keen eye for optimizing performance and ensuring seamless data flow, I bring a versatile skill set to deliver efficient and reliable backend functionalities.",
+      img: "https://static.vecteezy.com/system/resources/previews/015/600/206/non_2x/business-collaboration-icon-outline-style-vector.jpg",
+      style: {
+        backgroundColor: "#FA8072",
+        display: "flex",
+        flexDirection: "row",
+      },
+    },
+    {
+      Title: "Complete task within Deadline",
+      Msg: "Proficient in backend development, I leverage the power of Node.js and .NET to build robust and scalable server-side solutions. With a keen eye for optimizing performance and ensuring seamless data flow, I bring a versatile skill set to deliver efficient and reliable backend functionalities.",
+      img: "https://static.vecteezy.com/system/resources/previews/017/571/160/non_2x/calendar-icon-calendar-deadline-or-event-reminder-isolated-illustration-vector.jpg",
+      style: {
+        backgroundColor: "#FA8072",
+        display: "flex",
+        flexDirection: "row",
       },
     },
   ];
@@ -30,11 +50,13 @@ const ServicesMain = () => {
       id="services"
       className="flex flex-col justify-center align-middle h-screen bg-gradient-to-r from-cyan-500 to-blue-500"
     >
-      <div className=" m-[10%] block h-1/2">
-        <p className="text-3xl text-center">What I can Do</p>
-        {services.map((service, index) => {
-          return <ServiceItems key={index} item={service}></ServiceItems>;
-        })}
+      <div className="  flex flex-col items-center h-1/2">
+        <p className="text-3xl text-center my-2">What I can Do</p>
+        <div className=" grid grid-cols-1 lg:grid-cols-2 lg:gap-4">
+          {services.map((service, index) => {
+            return <ServiceItems key={index} item={service}></ServiceItems>;
+          })}
+        </div>
       </div>
     </div>
   );
